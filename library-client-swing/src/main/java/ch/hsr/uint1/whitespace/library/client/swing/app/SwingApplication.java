1cd92edec67fb75b002e5363a78da9a0342658f3
Roberto Cuervo <rcuervoa@hsr.ch>
2014-10-15 13:08:15 +0200

    Look & Feel native

--- library-client-swing/src/main/java/ch/hsr/uint1/whitespace/library/client/swing/app/SwingApplication.java
+++ library-client-swing/src/main/java/ch/hsr/uint1/whitespace/library/client/swing/app/SwingApplication.java
@@ -1,21 +1,31 @@
 package ch.hsr.uint1.whitespace.library.client.swing.app;
 
+import javax.swing.UIManager;
+
 import ch.hsr.uint1.whitespace.library.client.swing.bl.Library;
 import ch.hsr.uint1.whitespace.library.client.swing.dl.LibraryData;
 import ch.hsr.uint1.whitespace.library.client.swing.views.GadgetMaster;
 
 public class SwingApplication {
 
-	private Library library;
-	private GadgetMaster masterFrame;
+    private final Library library;
+    private GadgetMaster masterFrame;
 
-	public SwingApplication(LibraryData libraryData) {
-		this.library = new Library(libraryData);
-	}
+    public SwingApplication(final LibraryData libraryData) {
+        library = new Library(libraryData);
+    }
 
-	public void showMasterGUI() {
-		this.masterFrame = new GadgetMaster(library);
-		this.masterFrame.setVisible(true);
-	}
+    public void showMasterGUI() {
+
+        try {
+            // Set System L&F
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        masterFrame = new GadgetMaster(library);
+        masterFrame.setVisible(true);
+    }
 
 }
