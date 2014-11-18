285e307eaa41003a99f303e05bf9d44c9f5da2e2
Roberto Cuervo <rcuervoa@hsr.ch>
2014-11-18 19:26:59 +0100

    Removed unsued Labels. MainView Activity added

---
+++ library-client-android/src/ch/hsr/uint1/whitespace/library/client/android/activities/MainViewActivity.java
@@ -0,0 +1,38 @@
+package ch.hsr.uint1.whitespace.library.client.android.activities;
+
+import ch.hsr.uint1.whitespace.library.client.android.R;
+import ch.hsr.uint1.whitespace.library.client.android.R.id;
+import ch.hsr.uint1.whitespace.library.client.android.R.layout;
+import ch.hsr.uint1.whitespace.library.client.android.R.menu;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainViewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_view);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_view, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
