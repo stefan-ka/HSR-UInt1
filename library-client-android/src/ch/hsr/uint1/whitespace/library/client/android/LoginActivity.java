779e3d5a8bac83d3d7617864091a68498e639f25
Roberto Cuervo <rcuervoa@hsr.ch>
2014-11-18 09:39:35 +0100

    Login and register activities added

---
+++ library-client-android/src/ch/hsr/uint1/whitespace/library/client/android/LoginActivity.java
@@ -0,0 +1,38 @@
+package ch.hsr.uint1.whitespace.library.client.android;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void createRegisterActivity(View view) {
+		Intent intent = new Intent(this, RegisterActivity.class);
+		startActivity(intent);
+    }
+
+}
---
+++ library-client-android/src/ch/hsr/uint1/whitespace/library/client/android/RegisterActivity.java
@@ -0,0 +1,19 @@
+package ch.hsr.uint1.whitespace.library.client.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class RegisterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register_activity);
+		
+	}
+	
+	
+	
+}
