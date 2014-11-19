2a1d88ecced004bee52dce7d3dd5f082a9a5d1e7
Roberto Cuervo <rcuervoa@hsr.ch>
2014-11-19 09:58:07 +0100

    Icon added to List view item

--- library-client-android/src/ch/hsr/uint1/whitespace/library/client/android/activities/LoginActivity.java
+++ library-client-android/src/ch/hsr/uint1/whitespace/library/client/android/activities/LoginActivity.java
@@ -1,4 +1,5 @@
 package ch.hsr.uint1.whitespace.library.client.android.activities;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -6,20 +7,20 @@ import ch.hsr.uint1.whitespace.library.client.android.R;
 
 public class LoginActivity extends CommonActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_activity);
+	}
 
-    public void createRegisterActivity(View view) {
+	public void createRegisterActivity(View view) {
 		Intent intent = new Intent(this, RegisterActivity.class);
 		startActivity(intent);
-    }
+	}
 
-    public void createMainViewActivity(View view) {
-    		Intent intent = new Intent(this, MainViewActivity.class);
-    		startActivity(intent);
-        }
-    
+	public void createMainViewActivity(View view) {
+		Intent intent = new Intent(this, ListViewActivity.class);
+		startActivity(intent);
+	}
+
 }
